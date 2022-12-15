25136d0742960388c09f881272f3eb601b80e1f2
Domains18 <gibsonsgibson88@gmail.com>
2022-12-15 21:30:58 +0300

    typescript structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import{ Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import { Home } from './Pages/Home';
+import { Store } from './Pages/Store';
+import { About } from './Pages/About';
+import { Navbar } from './Components/Navbar';
 function App() {
 
   return (
-    <div>
-      rendering
-    </div>
+    <Container className='mb-4'>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="store" element={<Store/>} />
+        <Route path="about" element={<About/>} />
+      </Routes>
+    </Container>
   )
 }
 
---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,8 @@
+import { Container, Navbar as NavbarImport } from 'react-bootstrap';
+export function Navbar (){
+    return (
+        <NavbarImport className='bg-white shadow-sm mb-3'>
+            <Container> nav </Container>
+        </NavbarImport>
+    );
+};
